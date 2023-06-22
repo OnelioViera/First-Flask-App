ce498e5ba4fe518e1c42aa3f35d6de18d94055a4
ojvwe <ojvwebdesign@gmail.com>
2023-06-22 08:18:25 -0400

    updated files

--- app.py
+++ app.py
@@ -1,31 +1,50 @@
+from flask import Flask, request, render_template, redirect, flash, jsonify
+
 from random import randint, choice, sample
-from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "chickenzarecoolk21837"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
+
+MOVIES = {"Add Movies"}
 
 
 @app.route("/")
 def home_page():
-    html = """
-    <html>
-    <body>
-    <h1>Home Page</h1>
-    <p>Welcome to my simple app!</p>
-    <a href='/'>Home Page</a>
-    <br>
-    <a href='/hello'>Go to the hello page</a>
-    <br>
-    <a href='/goodby'>Go to the goodby page</a>
-    <br>
-    <a href='/add-comment'>Add Comment</a>
-    </body>
-    </html>
-    """
-    return html
+    """Shows home page"""
+    return render_template("home.html")
+
+
+@app.route("/old-home-page")
+def redirect_to_home():
+    """Redirects to new home page"""
+    return redirect("/")
+
+
+@app.route("/movies")
+def show_all_movies():
+    """Show list of all movies in fake DB"""
+    return render_template("movies.html", movies=MOVIES)
+
+
+@app.route("/movies/json")
+def get_movies_json():
+    return jsonify(list(MOVIES))
+
+
+@app.route("/movies/new", methods=["POST"])
+def add_movie():
+    title = request.form["title"]
+    # Add to pretend DB
+    if title in MOVIES:
+        flash("Movie Already Exists!", "error")
+    else:
+        MOVIES.add(title)
+        flash("Created Your Movie!", "success")
+    return redirect("/movies")
 
 
 @app.route("/form")
@@ -65,96 +84,3 @@ def get_greeting_2():
 def lucky_number():
     num = randint(1, 10)
     return render_template("lucky.html", lucky_num=num, msg="You are so lucky!")
-
-
-@app.route("/spell/<word>")
-def spell_word(word):
-    caps_word = word.upper()
-    return render_template("spell_word.html", word=caps_word)
-
-
-@app.route("/add-comment")
-def add_comment_form():
-    return """
-   
-    <h1>Add Comment</h1>
-    <form method='POST'>
-    <input type='text' placeholder='comment' name='comment'/>
-    <br>
-    <input type='text' placeholder='username' name='username'/>
-    <br>
-    <button>Submit</button>
-    </form>
-    <a href='/'>Home Page</a>
-   
-"""
-
-
-@app.route("/add-comment", methods=["POST"])
-def save_comment():
-    comment = request.form["comment"]
-    username = request.form["username"]
-    return f"""
-    <h1>Saved Your Comment</h1>
-    <ul>
-        <li>Username: {username}</li>
-        <li>Comment: {comment}</li>        
-    </ul>
-    <a href='/'>Home Page</a>
-"""
-
-
-@app.route("/hello")
-def say_hello():
-    """Shows hello apge"""
-    return render_template("hello.html")
-
-
-@app.route("/goodbye")
-def say_goodbye():
-    html = """
-    <html>
-    <body>
-    <h1>Goodbye!</h1>
-    <p>This is the goodbye page</p>
-    <a href='/'>Home Page</a>
-    <br>
-    <a href='/hello'>Go to the hello page</a>
-    </body>
-    </html>
-    """
-    return html
-
-
-@app.route("/search")
-def search():
-    term = request.args["term"]
-    sort = request.args["sort"]
-    return f"<h1>Search Results For: {term}</h1> <p>Sorting By: {sort}</p>"
-
-
-# @app.route("/post", methods=["POST"])
-# def post_demo():
-#     return "You Made A Post Request!"
-
-# @app.route("/post", methods=["GET"])
-# def get_demo():
-#     return "You Made A Get Request!"
-
-# @app.route("/add-comment")
-# def add_comment_form():
-#     return """
-
-#     <h1>Add Comment</h1>
-#     <form method='POST'>
-#     <input type='text' placehloder='comment' name='comment'/>
-#     <button>Submit</button>
-#     </form>
-
-# """
-
-# @app.route("/add-comment", methods=["POST"])
-# def save_comment():
-#     return """
-#     <h1>Saved Your Comment!</h1>
-# """
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ojvwe/FirstFlaskApp/venv"
+set -gx VIRTUAL_ENV "/mnt/c/Users/ojvwe/OneDrive/Documents/GitHub/First-Flask-App/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
